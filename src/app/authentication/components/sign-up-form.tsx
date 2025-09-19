14355656115a3f3b9d3b3eec81059fd5b14ffe3d
Nicolas Saraiva <nicolassaraiva@MacBook-Air-de-Nicolas.local>
2025-09-19 16:32:09 -0300

    feat: add sign out

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -71,7 +71,7 @@ const SignUpForm = () => {
 
   return (
     <>
-      <Card>
+      <Card className="w-full">
         <CardHeader>
           <CardTitle>Criar conta</CardTitle>
           <CardDescription>Crie uma conta para continuar.</CardDescription>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 "use client"
-import { useRouter } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
+import Header from "@/components/common/header";
 
 export default function Home() {
-  const navigate = useRouter()
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button onClick={() => navigate.push("/authentication")}>Click me</Button>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
