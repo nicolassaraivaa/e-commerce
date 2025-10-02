9833ae5da20acafbbd57478f8dbc62fb7564a7d3
Nicolas Saraiva <nicolassaraiva@MacBook-Air-de-Nicolas.local>
2025-10-02 20:21:40 -0300

    feat: ensure stripe session is created

--- src/actions/finish-order/index.ts
+++ src/actions/finish-order/index.ts
@@ -41,6 +41,9 @@ export const finishOrder = async () => {
     (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
     0,
   );
+
+  let orderId: string | undefined;
+
   await db.transaction(async (tx) => {
     if (!cart.shippingAddress) {
       throw new Error("Shipping address not found");
@@ -68,6 +71,7 @@ export const finishOrder = async () => {
     if (!order) {
       throw new Error("Failed to create order");
     }
+    orderId = order.id;
     const orderItemsPayload: Array<typeof orderItemTable.$inferInsert> =
       cart.items.map((item) => ({
         orderId: order.id,
@@ -79,4 +83,9 @@ export const finishOrder = async () => {
     await tx.delete(cartItemTable).where(eq(cartItemTable.cartId, cart.id));
     await tx.delete(cartTable).where(eq(cartTable.id, cart.id));
   });
+  if (!orderId) {
+    throw new Error("Failed to create order");
+  }
+
+  return { orderId };
 };
--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,31 +1,57 @@
 "use client";
 
+import { loadStripe } from "@stripe/stripe-js";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
+import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogFooter,
-  DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 
 const FinishOrderButton = () => {
-  const [sucessDialogIsOpen, setSucessDialogIsOpen] = useState<boolean>(true);
+  const [sucessDialogIsOpen, setSucessDialogIsOpen] = useState<boolean>(false);
   const finishOrderMutation = useFinishOrder();
+
+  const handleFinishOrder = async () => {
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Stripe publishable key is not set");
+    }
+
+    const { orderId } = await finishOrderMutation.mutateAsync();
+    const checkoutSession = await createCheckoutSession({
+      orderId,
+    });
+
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+
+    if (!stripe) {
+      throw new Error("failed to load stripe");
+    }
+
+    await stripe.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+
+    setSucessDialogIsOpen(true);
+  };
 
   return (
     <>
       <Button
         className="w-full rounded-full"
         size={"lg"}
-        onClick={() => finishOrderMutation.mutate()}
+        onClick={handleFinishOrder}
         disabled={finishOrderMutation.isPending}
       >
         {finishOrderMutation.isPending && (
@@ -53,7 +79,7 @@ const FinishOrderButton = () => {
               Ver meus pedidos
             </Button>
             <Button className="rounded-full" size={"lg"} variant={"outline"}>
-              Voltar para a loja
+              <Link href={"/"}>Voltar para a loja</Link>
             </Button>
           </DialogFooter>
         </DialogContent>
